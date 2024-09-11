dd7f7ec1a97f5383463fa1156f1b9282c8460295
TomasHoncik <147615167+TomasHoncik@users.noreply.github.com>
2024-09-11 14:02:04 +0200

    Update knihovna.py

--- knihovna.py
+++ knihovna.py
@@ -7,7 +7,7 @@ prihlasen = ""
 seznam_uzivatelu = {
     1: {"jmeno": "Jana", "heslo": "kocka", "pujcene_knihy": []},
     2: {"jmeno": "Martin", "heslo": "auto", "pujcene_knihy": [ "1984"]},
-    3: {"jmeno": "Lucie", "heslo": "strom", "pujcene_knihy": ["velkygatsby"]},
+    3: {"jmeno": "Lucie", "heslo": "strom", "pujcene_knihy": []},
     4: {"jmeno": "Petr", "heslo": "pes", "pujcene_knihy": []},
     5: {"jmeno": "Jirka", "heslo": "sova", "pujcene_knihy":  ["zlocinatrest"]},
     6: {"jmeno": "Hana", "heslo": "leto", "pujcene_knihy": []},
@@ -23,21 +23,50 @@ seznam_knih = {
     "1984": {"autor": "George Orwell", "ISBN": "9780451524935", "pujceno": False},
     "hobit": {"autor": "J.R.R. Tolkien", "ISBN": "9780261103344", "pujceno": False},
     "zlocinatrest": {"autor": "Fjodor Dostojevskij", "ISBN": "9780143058144", "pujceno": True},
-    "velkygatsby": {"autor": "F. Scott Fitzgerald", "ISBN": "9780743273565", "pujceno": False},
+    "velkygatsby": {"autor": "F. Scott Fitzgerald", "ISBN": "9780743273565", "pujceno": True},
     "prideandprejudice": {"autor": "Jane Austen", "ISBN": "9781503290563", "pujceno": True},
     "annakarenina": {"autor": "Lev Tolstoj", "ISBN": "9780199232086", "pujceno": False}
 }
 
-def odstranit_knihu():
+def odstranit_knihu()
     nazev = input("Zadej název knihy kterou chceš odstranit: ")
 
+    text = unicodedata.normalize('NFD', nazev)
+    text_bez_diakritiky = ''.join(char for char in text if unicodedata.category(char) != 'Mn')
+    text_bez_diakritiky_mala_pismena = text_bez_diakritiky.lower().replace(" ", "")
+
+    if text_bez_diakritiky_mala_pismena in seznam_knih:
+        print("test")
+        del seznam_knih[text_bez_diakritiky_mala_pismena]
+        print(seznam_knih)
+
+    else:
+        print("Taková kniha tu není")
+        return odstranit_knihu()
+    
+    menu()
+
+def pridat_knihu():
+    nazev = input("zadej název knihy: ")
+    autor = input("zadej autora knihy: ")
+    isbn = input("zadej ISBN knihy: ")
+
+    text = unicodedata.normalize('NFD', nazev)
+    text_bez_diakritiky = ''.join(char for char in text if unicodedata.category(char) != 'Mn')
+    text_bez_diakritiky_mala_pismena = text_bez_diakritiky.lower().replace(" ", "")
+
+    if text_bez_diakritiky_mala_pismena in seznam_knih:
+        print("Tato kniha už v knihovně je!")
+        return pridat_knihu()
+    else:
+        seznam_knih[text_bez_diakritiky_mala_pismena] = {"autor": autor,"ISBN":isbn, "pujceno":False}
+        print("Kniha byla úspěšně přidána")
+        
+    menu()
 
 
 def vyhledat():
-
     global prihlasen
-
-
     hledat = input("Zadej název knihy: ")
 
     text = unicodedata.normalize('NFD', hledat)
@@ -50,8 +79,8 @@ def vyhledat():
         for uzivatel in seznam_uzivatelu.values():
             if uzivatel["jmeno"] == prihlasen:
                 uzivatel["pujcene_knihy"].append(text_bez_diakritiky_mala_pismena)
-        print("kniha byla půjčena")
-        print (seznam_uzivatelu)
+                print(seznam_knih)
+                print("kniha byla půjčena")
 
     elif text_bez_diakritiky_mala_pismena in seznam_knih.keys() and seznam_knih[text_bez_diakritiky_mala_pismena]["pujceno"] == True:
         print("Tato kniha je bohužel již půjčena")
@@ -59,14 +88,23 @@ def vyhledat():
     elif text_bez_diakritiky_mala_pismena not in seznam_knih.keys():
         print("Takovou knihu tu nemáme")
         return vyhledat()
+    
+    menu()
 
 
 def menu():
-    volba = int(input("[1] Půjčit  knihu [2] Odstranit knihu [3 přidat knihu]: "))
+    volba = int(input("[1] Půjčit  knihu [2] Odstranit knihu [3] přidat knihu [4] Vypsat dostupné knihy: "))
     if volba == 1:
         vyhledat()
-    elif volba = 2:
+    elif volba == 2:
+        odstranit_knihu()
+    elif volba == 3:
+        pridat_knihu()
+    elif volba == 4:
+        print(seznam_knih)
+
         
+    
 
 
 def login():
@@ -79,8 +117,12 @@ def login():
         if jmeno == uzivatel["jmeno"] and heslo == uzivatel["heslo"]:
             prihlasen = jmeno
             print("Přihlášení proběhlo úspěšně")
-        
-    vyhledat()
+            break
+        else:
+            print("špatné heslo nebo jméno!")
+            return login()
+
+    menu()
         
  
     
@@ -106,15 +148,18 @@ def register():
         seznam_uzivatelu[len(seznam_uzivatelu) + 1] = {"jmeno": jmeno, "heslo": heslo_1, "pujcene_knihy": []}
         print("registrace proběhla v pořádku")
         print(seznam_uzivatelu)
+
+    
+    menu()
         
 while True:
     volba = int(input("[1] Přihlásit se [2] Zaregistrovat se: "))
     if volba == 1:
         login()
+        menu()
     elif volba == 2:
         register()
     else:
         print("špatná volba")
         continue
-
 
